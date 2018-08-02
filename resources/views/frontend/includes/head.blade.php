038b1f962d6beaf3d2403ef0258e7fe399f5dd1d
binkaa2 <binkaa1@gmail.com>
2018-08-02 14:13:02 +0700

    update jquery

--- resources/views/frontend/includes/head.blade.php
+++ resources/views/frontend/includes/head.blade.php
@@ -20,6 +20,9 @@
     <!--khai bao duong dan-->
     <base href="{{ asset('')}}">
 
+    <!--jquery-->
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
