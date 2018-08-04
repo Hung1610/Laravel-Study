4383730d9a5ad0bf94da2be5df55198ef846be43
binkaa2 <binkaa1@gmail.com>
2018-08-04 12:06:43 +0700

    edit index page

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Content;
 class mainController extends Controller
 {
+    public function __construct(Content $model){
+      return $this->model=$model;
+    }
     public function getTrangChu(){
-      return view('frontend.index');
+      return view('frontend.index',['data_content'=>$this->model::paginate(10)]);
     }
     public function getGioiThieu(){
       return view('frontend.pages.gioithieu');
