1aa9189780ed13741d96f1c69f86de9040043edb
binkaa2 <binkaa1@gmail.com>
2018-08-04 22:29:54 +0700

    minor fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -12,13 +12,8 @@
                   <a class="aindex" href="{{ url($key->alias.'/'.$key->id)}}"><img src="{{$key->img}}" alt="{{$key->title}}"></a>
 		             </div>
 							<div class="col-md-7">
-<<<<<<< HEAD
                 <a href="{{route('pagedetail',[$key->alias,$key->id])}}"><p>{{$key->title}}</p></a>
 								{!!  substr(strip_tags($key->content), 0, 204) !!}
-=======
-                <a href=""><p>{{$key->title}}</p></a>
-								{!!  substr(strip_tags($key->content), 0, 204) . "..." !!}
->>>>>>> d7a3ede8006448bf82f7245d592370bc732d6813
 							</div>
 				       <div class="break"></div>
 		          </div>
