0b36dd920a1dc4fcedefa16780658e0df83f3ff7
binkaa2 <binkaa1@gmail.com>
2018-08-04 13:08:05 +0700

    tittle icon

--- resources/views/frontend/includes/head.blade.php
+++ resources/views/frontend/includes/head.blade.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="image/index/icon.png">
 
     <title>test</title>
 
